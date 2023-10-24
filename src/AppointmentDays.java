528e24e4bca724c0af2771b007d590a051bb4735
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2023-10-24 11:26:30 +0200

    1

--- src/AppointmentDays.java
+++ src/AppointmentDays.java
@@ -1,4 +1,22 @@
 package src;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class AppointmentDays {
+    private LocalDate date;
+    private boolean weekend;
+    private Arraylist <Appointment> appointments;
+
+    public AppointmentDays(LocalDate date) {
+        this.date = date;
+        this.weekend = isWeekend();
+        this.appointments = new Arraylist <Appointment>();
+    }
+
+    public boolean isWeekend() {
+        DayOfWeek day = date.getDayOfWeek();
+        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+    }
 }
+
